ee47031b5f57339bbe4dd40acfd9b3757fda134d
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-05-28 15:16:44 +0200

    de basis template web
    Dit is de basis template maar hij zit nu in de repository

---
+++ resources/views/Schoolvoetbal.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<header>
+    <img src="{{ URL('img/4S-Logo.png') }}" alt="4S-Logo">
+    <a href="Schoolvoetbal.blade.php">Home</a>
+    <a href="Wedstrijd.blade.php">Wedstrijden</a>
+    <a href="Registeer.blade.php">Registeren</a>
+    <a href="Inlog.blade.php">Inloggen</a>
+
+</header>
+<body>
+    <h1>Schoolvoetbal</h1>
+    <img src="{{ URL('img/football.jpg') }}" alt="football">
+</body>
+<footer>
+    <p>Thijmen, Wouter, Nathan, Laurant, Jurgen</p>
+</footer>
+</html>
