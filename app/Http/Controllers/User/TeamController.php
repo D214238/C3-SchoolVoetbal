5d369761cfb15aa24dca325d441beae34361d9b6
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-06-15 12:52:39 +0200

    team controller
    team controller geschreven, maar nu nog hopen dat hij werkt.

--- app/Http/Controllers/User/TeamController.php
+++ app/Http/Controllers/User/TeamController.php
@@ -18,31 +18,40 @@ class TeamController extends Controller
 
     public function create()
     {
-        //
+        return view("teams.create");
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'=> 'required',
+            'creator_id'=> 'required',
+        ]);
+        Team::create($request->all());
+
+        return redirect()->route('teams.index')->with('succes','Team succesvol aangemaakt!');
+                        
     }
 
-    public function show($id)
+    public function show(Team $team)
     {
-        //
+        return view('teams.show',compact('teams'));
     }
 
-    public function edit($id)
+    public function edit(Team $team)
     {
-        //
+        return view('teams.edit',compact('team'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Team $team)
     {
-        //
+        return redirect()->route('teams.index')->with('Succes','Team succesvol aangepast!');
     }
 
-    public function destroy($id)
+    public function destroy(Team $team)
     {
-        //
+        $team->delete();
+
+        return redirect()->route('teams.index')->with('Succes','Team succesvol gewist!');
     }
 }
