7b5bde0b079f1cc914a50bdb0afd3a54a72b3f97
Laurant Marijnissen <laurant@gigafyde.dev>
2022-06-21 14:16:12 +0200

    Add beginning of api routes

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Use App\Models\Game;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('matches', function() {
+    return Game::all();
 });
