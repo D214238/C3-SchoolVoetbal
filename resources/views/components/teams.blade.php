a6e89f7c28403a459a6a19b1cc53d8c506325ad3
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-06-22 12:18:21 +0200

    team table

--- resources/views/components/teams.blade.php
+++ resources/views/components/teams.blade.php
@@ -1,11 +1,20 @@
 @props(['teams'])
-<div {{ $attributes->merge(['class' => 'comp-tournaments']) }}>
+<div {{ $attributes->merge(['class' => 'comp-teams']) }}>
     <table class="main-table">
-        <thead class="tournaments-header">
+        <thead class="teams-header">
         @foreach($teams as $team)
+            
+            <tr>
+                <th>name</th>
+                <th>created at</th>
+                <th>updated at</th>
+                <th>creator id</th>
+            </tr>
             <tr>
                 <td>{{ $team['name'] }}</td>
-
+                <td>{{ $team['created_at'] }}</td>
+                <td>{{ $team['updated_at'] }}</td>
+                <td>{{ $team['creator_id'] }}</td>
             </tr>
         @endforeach
 
