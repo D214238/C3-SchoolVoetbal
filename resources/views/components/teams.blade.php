a645e797ed27fa2d62c59bc90f1004a8e6041fa2
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-06-27 13:14:43 +0200

    buttons

--- resources/views/components/teams.blade.php
+++ resources/views/components/teams.blade.php
@@ -1,13 +1,16 @@
 @props(['teams'])
-      
+
 <div {{ $attributes->merge(['class' => 'comp-teams']) }}>
     <table class="main-table">
+    <a class="btn btn-success" href="{{ route('teams.create') }}"> Create New Team</a>
         <thead class="teams-header">
             <tr>
                 <th>name</th>
                 <th>create_at</th>
                 <th>update_at</th>
                 <th>creator_id</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
         @foreach($teams as $team)
             
@@ -18,6 +21,8 @@
                 <td>{{ $team['created_at'] }}</td>
                 <td>{{ $team['updated_at'] }}</td>
                 <td>{{ $team['creator_id'] }}</td>
+                <td><a class="btn btn-success" href="{{ route('teams.create') }}">Edit Team</a></td>
+                <td><a class="btn btn-success" href="{{ route('teams.create') }}">Delete Team</a></td>
             </tr>
         @endforeach
         </tbody>
---
+++ resources/views/user/teams/create.blade.php
@@ -0,0 +1,12 @@
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Team</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('Teans.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
