e3cfa440be33acbeca0593f9c090549c5c54659d
Laurant Marijnissen <laurant@gigafyde.dev>
2022-06-28 14:58:20 +0200

    Properly implement the first 2 api routes

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Use App\Models\Game;
+Use App\Models\Team;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('matches', function() {
-    return Game::all();
+    $games = Game::all();
+    $results = [];
+    foreach ($games as $game) {
+        $result = [];
+        $result["id"] = $game['id'];
+        $result["team1_id"] = $game['team1_id'];
+        $result["team1_name"] = Team::where('id',$result["team1_id"])->get("name")[0]['name'];
+        $result["team2_id"] = $game['team2_id'];
+        $result["team2_name"] = Team::where('id',$result["team2_id"])->get("name")[0]['name'];
+        $results[] = $result;
+    }
+    return $results;
+});
+
+Route::get('results', function() {
+    $games = Game::all();
+    $results = [];
+    foreach ($games as $game) {
+        $result = [];
+        $result["team1_id"] = $game['team1_id'];
+        $result["team1_name"] = Team::where('id',$result["team1_id"])->get("name")[0]['name'];
+        $result["team1_score"] = $game['team1_score'];
+        $result["team2_id"] = $game['team2_id'];
+        $result["team2_name"] = Team::where('id',$result["team2_id"])->get("name")[0]['name'];
+        $result["team2_score"] = $game['team2_score'];
+        if ($result["team1_score"] > $result["team2_score"]) $result["winner_id"] = $game['team1_id'];
+        else $result["winner_id"] = $result["team2_id"];
+        $results[] = $result;
+    }
+    return $results;
+
 });
