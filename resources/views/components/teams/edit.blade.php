7285681408170b0813649552433ba946c3009fe5
Laurant Marijnissen <laurant@gigafyde.dev>
2022-07-04 13:02:29 +0200

    Fix edit of teams

--- resources/views/components/teams/edit.blade.php
+++ resources/views/components/teams/edit.blade.php
@@ -13,10 +13,12 @@
         <input type="text" name="creator_id" id="creator_id" value="{{$team->creator_id}}" class="form-control"><br/>
         <br/>
         <button type="submit" class="btn btn-success">Update</button>
-        <form action="{{url('admin/teams', [$team])}}" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input style="background-color: #000000; color: #ffffff; font-size: 30px; padding: 5px 15px;" type="submit" class="btn btn-danger" value="Delete"/>
-        </form>
+
+
+    </form>
+    <form action="{{url('admin/teams', [$team])}}" method="POST">
+        <input type="hidden" name="_method" value="DELETE">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input style="background-color: red; color: #780000; font-size: 30px; padding: 5px 15px;" type="submit" class="btn btn-danger" value="Delete"/>
     </form>
 </div>
